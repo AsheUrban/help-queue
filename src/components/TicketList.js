ee1e209f270ac188d32ad0c526f7550ed0f65579
Ashe Urban <ashe@goldentongue.com>
2022-09-07 12:00:38 -0700

    Finish lesson 32. Queue still not visible.
    Co-authored-by: matt wilkinson <wilkinsonmatt01@gmail.com>

--- src/components/TicketList.js
+++ src/components/TicketList.js
@@ -2,25 +2,27 @@ import React from "react";
 import Ticket from "./Ticket";
 import PropTypes from "prop-types";
 
-// remove const mainTicketList = [ ... ]. We no longer want these.
+function TicketList(props){
 
-function TicketList(props) { // Add props as parameter.
   return (
     <React.Fragment>
-      <hr />
-      {props.ticketList.map((ticket, index) => // Loop through the list passed down from TicketControl.
-        <Ticket names={ticket.names}
+      <hr/>
+      {props.ticketList.map((ticket) =>
+        <Ticket
+          whenTicketClicked = { props.onTicketSelection }
+          names={ticket.names}
           location={ticket.location}
           issue={ticket.issue}
-          key={index} />
+          id={ticket.id}
+          key={ticket.id}/>
       )}
     </React.Fragment>
   );
 }
 
-// Add propTypes for ticketList.
 TicketList.propTypes = {
-  ticketList: PropTypes.array
+  ticketList: PropTypes.array,
+  onTicketSelection: PropTypes.func
 };
 
 export default TicketList;
